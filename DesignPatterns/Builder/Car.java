30fda2b4c342c3291ad5196c42cd9d20f4d63f14
DHuber10 <47402664+DHuber10@users.noreply.github.com>
2023-10-25 22:31:17 -0500

    Create Car.java

---
+++ DesignPatterns/Builder/Car.java
@@ -0,0 +1,54 @@
+package OOPFundamentals.Builder;
+
+public class Car {
+
+    private final int id;
+    private final int height;
+    private final String brand;
+    private final String model;
+    private final String color;
+    private final String engine;
+    private final int nbrOfDoors;
+
+    public Car(int id, String brand, String model, String color, int height, String engine, int nbrOfDoors) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.height = height;
+        this.engine = engine;
+        this.nbrOfDoors = nbrOfDoors;
+     }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public int getNbrOfDoors() {
+        return nbrOfDoors;
+    }
+
+        
+
+}
+    
